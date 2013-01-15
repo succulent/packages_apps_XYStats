bb61045102a1a6c4578d1d70666efa6788d8f41c
fuss132 <homepagechecker@gmx.de>
2013-01-15 15:55:51 +0100

    make really work on xylon - there are now only values in build.prop like ro.xylon.isitrom

--- src/com/fuss/stats/Constants.java
+++ src/com/fuss/stats/Constants.java
@@ -3,11 +3,11 @@ package com.fuss.stats;
 public class Constants 
 {
 	// These are used to check values from build.prop
-	public static final String ISITROM = "ro.fuss132.isitrom";
-	public static final String MODNAME = "ro.fuss132.modname";
-	public static final String MODVERSION = "ro.fuss132.modversion";
-	public static final String DEVICE = "ro.fuss132.device";
-	public static final String BUILTDATE = "ro.fuss132.builtdate";
+	public static final String ISITROM = "ro.xylon.isitrom";
+	public static final String MODNAME = "ro.xylon.modname";
+	public static final String MODVERSION = "ro.xylon.modversion";
+	public static final String DEVICE = "ro.xylon.device";
+	public static final String BUILTDATE = "ro.xylon.builtdate";
 	public static final String SPLIT_EXPRESSION = "=";
 	
 	//These are used only for xml header fields
